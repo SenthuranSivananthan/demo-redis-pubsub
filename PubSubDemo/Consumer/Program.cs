f25b1e67a38bb8da32e5b7e10ccea247c1ce0cdd
Senthuran Sivananthan [MSFT] <sesivan@microsoft.com>
2016-08-30 11:51:45 -0400

    Basic example of using pub/sub model with Redis Cache

---
+++ PubSubDemo/Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Configuration;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(ConfigurationManager.AppSettings["Redis.ConnectionString"]);
+
+            // By default, messages are processed sequentially.
+            // See https://github.com/StackExchange/StackExchange.Redis/blob/master/Docs/PubSubOrder.md
+            connection.PreserveAsyncOrder = true;
+
+            ISubscriber subscriber = connection.GetSubscriber();
+
+            RedisChannel redisChannel = new RedisChannel("CachedDataRefresh", RedisChannel.PatternMode.Literal);
+            subscriber.SubscribeAsync(
+                                            redisChannel,
+                                            (channel, value) =>
+                                            {
+                                                Console.WriteLine("Received Notification - Channel: {0}, Value: {1}", channel, value);
+                                            });
+
+
+            Console.WriteLine("Listening for new notifications.  Press any key to terminate.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PubSubDemo/Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Configuration;
+using System.Threading;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(ConfigurationManager.AppSettings["Redis.ConnectionString"]);
+
+            RedisChannel redisChannel = new RedisChannel("CachedDataRefresh", RedisChannel.PatternMode.Literal);
+            ISubscriber subscriber = connection.GetSubscriber();
+
+            while (true)
+            {
+                var message = string.Format("Hello-{0}", DateTime.Now.ToString("hh:mm:ss"));
+                subscriber.Publish(redisChannel, message);
+
+                Console.WriteLine("Published: {0}", message);
+
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
